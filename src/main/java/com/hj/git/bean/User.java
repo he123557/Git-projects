150a7bd591af2cb5dee57ce23943ef5fe7005212
hj <hj@git.com>
2023-02-11 14:55:16 +0800

    idea second commit

--- src/main/java/com/hj/git/bean/User.java
+++ src/main/java/com/hj/git/bean/User.java
@@ -6,13 +6,15 @@ package com.hj.git.bean;
 public class User {
     private Integer id;
     private String name;
+    private Integer age;
 
     public User() {
     }
 
-    public User(Integer id, String name) {
+    public User(Integer id, String name,Integer age) {
         this.id = id;
         this.name = name;
+        this.age=age;
     }
 
     public Integer getId() {
@@ -31,11 +33,20 @@ public class User {
         this.name = name;
     }
 
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", age=" + age +
                 '}';
     }
 }
