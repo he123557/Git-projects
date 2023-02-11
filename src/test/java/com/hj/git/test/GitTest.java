9ca646998872e2e9678b2e32847f3bcf3c541993
hj <hj@git.com>
2023-02-11 15:01:01 +0800

    idea third commit

---
+++ src/test/java/com/hj/git/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.hj.git.test;
+
+/**
+ * @create 2023-02-11 14:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+        System.out.println("hello,git!");
+        System.out.println("hello,git!");
+    }
+}
